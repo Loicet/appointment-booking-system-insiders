fd277d47fa04c055219901901d00890f87429fb6
Loicet <l.teta2@alustudent.com>
2025-11-14 10:35:17 +0200

    Added the doctor details page

--- appointment-booking/src/App.jsx
+++ appointment-booking/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/home";
 import BookDoctor from "./pages/BookDoctor";
+import DoctorDetails from "./pages/DoctorDetails";
+
 
 
 
