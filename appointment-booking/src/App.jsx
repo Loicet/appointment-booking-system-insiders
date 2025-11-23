79e4b800221bc8b9ee14e402c096e8e354361e5c
Loicet <l.teta2@alustudent.com>
2025-11-23 10:02:59 +0200

    Finalized app version

--- appointment-booking/src/App.jsx
+++ appointment-booking/src/App.jsx
@@ -1,19 +1,79 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+// Layouts
+import MainLayout from "./layout/MainLayout";
+import PatientLayout from "./layout/patientLayout";
+
+// Pages
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import BookDoctor from "./pages/BookDoctor";
-import DoctorDetails from "./pages/DoctorDetails";
-
-
-
+import Chat from "./pages/Chat";
+import AIChatbot from "./pages/AIChatbot";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          {/* Website Pages */}
+          <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+          <Route path="/services" element={<MainLayout><Services /></MainLayout>} />
+          <Route path="/about" element={<MainLayout><About /></MainLayout>} />
+          <Route path="/login" element={<MainLayout><Login /></MainLayout>} />
+          <Route path="/signup" element={<MainLayout><Signup /></MainLayout>} />
+
+          {/* Protected Dashboard Pages */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <PatientLayout>
+                  <Dashboard />
+                </PatientLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/book-doctor"
+            element={
+              <ProtectedRoute>
+                <PatientLayout>
+                  <BookDoctor />
+                </PatientLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <ProtectedRoute>
+                <PatientLayout>
+                  <Chat />
+                </PatientLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ai-chatbot"
+            element={
+              <ProtectedRoute>
+                <PatientLayout>
+                  <AIChatbot />
+                </PatientLayout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
